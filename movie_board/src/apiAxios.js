3deb4a69a0dbbc8f7db071e90f4b1bf604141284
GabinFiquet <gabin.fiquet@gmail.com>
2020-09-01 17:20:36 +0200

    Ajout de l appel à la base de film avec Axios

---
+++ movie_board/src/apiAxios.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: "http://localhost:3000/movies",
+    timeout: 30000,
+  });
+
+const apiAxios = {
+    init: async () => {
+        return instance.get("http://localhost:3000/movies").then((r) => r.data);
+    },
+    getData: async () => {
+        return instance.get("http://localhost:3000/movies").then((r) => {return r.data});
+    }
+}
+
+export default apiAxios;
