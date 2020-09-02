021fad128a175bcedd0a8d9c9e03fd08f617143c
GabinFiquet <gabin.fiquet@gmail.com>
2020-09-02 20:34:14 +0200

    Ajout de la banniere rouge sur pour la liste de film, changement du favicon et ajout de la police PT Sans

--- movie_board/src/components/movies/Movies.js
+++ movie_board/src/components/movies/Movies.js
@@ -1,10 +1,25 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import Movie from './Movie';
 import styles from './Movies.module.css';
 
 const Movies = (props) => {
     return (
         <div className={styles.moviesList}>
+            <div className={styles.redBanner}>
+                <div>
+                    <form>
+                        <input type="text" name="title"  placeholder="Titre" autoFocus/> 
+                        <input type="date" name="date"  autoFocus/>
+                        <input type="text" name="categories"  placeholder="Genre" autoFocus/>
+                        <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+                    </form>
+                    <a href='#'>Ajouter un film</a>
+                </div>
+            </div>
             {props.movies.map((movie) => (
                 <Movie key={movie.id} movie={movie} ></Movie>
             ))}
